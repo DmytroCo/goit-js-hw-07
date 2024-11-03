2d96fbcb4c3f9879a0cbd615ce5bf7cb5b23f654
Dmytro Popsha <popsha.dima@gmail.com>
2024-11-03 12:37:15 +0200

    update1

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+    const boxes = [];
+    
+    for (let i = 0; i < amount; i++) {
+        const size = 30 + i * 10; 
+        const box = document.createElement('div'); 
+        box.style.width = `${size}px`; 
+        box.style.height = `${size}px`; 
+        box.style.backgroundColor = getRandomHexColor(); 
+        box.style.margin = '5px'; 
+        boxes.push(box); 
+    }
+    
+    boxesContainer.append(...boxes); 
+}
+
+function destroyBoxes() {
+    boxesContainer.innerHTML = ''; 
+}
+
+createButton.addEventListener('click', () => {
+    const amount = Number(input.value); 
+    if (amount < 1 || amount > 100) {
+        alert('Please enter a number between 1 and 100.'); 
+        return; 
+    }
+    destroyBoxes(); 
+    createBoxes(amount); 
+    input.value = ''; 
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
